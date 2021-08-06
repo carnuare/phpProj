1639625e8ff5c5b0af19aad884524b43c42c0b71
carnuare <carnuare@gmail.com>
2021-08-06 12:05:37 +0200

    Change host installDB

--- .github/workflows/installDB.sh
+++ .github/workflows/installDB.sh
@@ -3,4 +3,4 @@ sudo apt install mysql-server
 echo "Running docker-compose"
 docker-compose up -d
 #telnet 127.0.0.1 3306 
-mysql -h 127.0.0.1 -u root assignment_tracker < assignment_tracker.sql --password=example
+mysql -h db -u root assignment_tracker < assignment_tracker.sql --password=example
