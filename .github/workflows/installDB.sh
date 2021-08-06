ee08ba20482228b899555c5560e6d7ae32aac486
carnuare <carnuare@gmail.com>
2021-08-06 12:12:27 +0200

    Update installDB

--- .github/workflows/installDB.sh
+++ .github/workflows/installDB.sh
@@ -2,5 +2,7 @@
 sudo apt install mysql-server
 echo "Running docker-compose"
 docker-compose up -d
+docker port YOUR_CONTAINER_NAME
+docker ps
 #telnet 127.0.0.1 3306 
 mysql -h db -u root assignment_tracker < assignment_tracker.sql --password=example
