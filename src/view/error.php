73d1edb8b6177d2d96dadfc91a1abde2dd71e59f
carnuare <carnuare@gmail.com>
2021-08-06 18:45:38 +0200

    Add db name in error message

--- src/view/error.php
+++ src/view/error.php
@@ -3,6 +3,7 @@
 <p><?php echo $error; 
         print "//////";
         print "NOTA: If you get a -connection refused- and you have just run docker-compose up, you should wait a couple of seconds and refresh the page.";
+        print "DB HOST:".getenv("MYSQL_HOST");
 ?></p>
 <br>
 <p><a href=".">Back to List</a></p>
