1ad570e7dce7e9971b33294fa5609ed271cb13de
carnuare <carnuare@gmail.com>
2021-08-06 19:35:56 +0200

    Update installDB.sh and workflow

--- .github/workflows/installDB.sh
+++ .github/workflows/installDB.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-
+sudo apt install mysql-server
+#telnet 127.0.0.1 3306 
+sleep 10
+mysql -u root --password=example 
+CREATE DATABASE assignment_tracker;
+EXIT
+mysql -u root assignment_tracker < assignment_tracker.sql --password=example 
