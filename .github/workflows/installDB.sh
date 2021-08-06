939b1fa47615edebbfb79a43d954110169f1a3e4
carnuare <carnuare@gmail.com>
2021-08-06 12:33:26 +0200

    Update installDB

--- .github/workflows/installDB.sh
+++ .github/workflows/installDB.sh
@@ -5,4 +5,7 @@ docker-compose up -d
 docker port YOUR_CONTAINER_NAME
 docker ps
 #telnet 127.0.0.1 3306 
-mysql -u root assignment_tracker < assignment_tracker.sql --password=example
+mysql -u root --password=example
+CREATE DATABASE assignment_tracker;
+EXIT
+mysql -h 127.0.0.1 -P 3306 -u root assignment_tracker < assignment_tracker.sql --password=example
