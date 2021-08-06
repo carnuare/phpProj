67df1992551d9e9262c6640776072cabcc10dc3b
carnuare <carnuare@gmail.com>
2021-08-06 19:53:12 +0200

    Update workflow

--- .github/workflows/installDB.sh
+++ .github/workflows/installDB.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 sudo apt install mysql-server
 sudo apt install mysql-client
+
 sleep 10
-mysql -u root assignment_tracker < assignment_tracker.sql --password=example 
+mysql --host db -u root assignment_tracker < assignment_tracker.sql --password=example 
