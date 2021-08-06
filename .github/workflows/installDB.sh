9fdcc27590a6c513052da3d6169a6c5df6480358
carnuare <carnuare@gmail.com>
2021-08-06 22:55:13 +0200

    Add sleep

--- .github/workflows/installDB.sh
+++ .github/workflows/installDB.sh
@@ -3,4 +3,5 @@ sudo apt install mysql-server
 sudo apt install mysql-client
 
 docker ps
+sleep 30
 mysql --host 127.0.0.1 --port 3306 -u root assignment_tracker < assignment_tracker.sql --password=example 
