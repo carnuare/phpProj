1cc990c38520aa75430126d69430eae7836724cb
carnuare <carnuare@gmail.com>
2021-08-06 12:01:56 +0200

    Change order

--- .github/workflows/installDB.sh
+++ .github/workflows/installDB.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 sudo apt install mysql-server
+echo "Running docker-compose"
+docker-compose up -d
 #telnet 127.0.0.1 3306 
 mysql -h 127.0.0.1 -u root assignment_tracker < assignment_tracker.sql --password=example
