6db540ebf1ee03fc2c4d04d026950432b8e0ee2e
carnuare <carnuare@gmail.com>
2021-08-06 22:44:06 +0200

    Uncomment

--- .github/workflows/installDB.sh
+++ .github/workflows/installDB.sh
@@ -2,5 +2,5 @@
 sudo apt install mysql-server
 sudo apt install mysql-client
 
-sleep 10
-mysql --host db -u root assignment_tracker < assignment_tracker.sql --password=example 
+sleep 30
+mysql --host db --port 3306 -u root assignment_tracker < assignment_tracker.sql --password=example 
