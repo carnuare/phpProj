eb25f4dbec39b76cb0b13cf3490d4f25a00ca7d3
carnuare <carnuare@gmail.com>
2021-11-18 13:40:16 +0100

    Change coverage

--- tests/phpunit/SampleTest.php
+++ tests/phpunit/SampleTest.php
@@ -2,11 +2,10 @@
 // TEST UNITARIO DE EJEMPLO
 use PHPUnit\Framework\TestCase;
 
-
 class SampleTest extends TestCase{
 
     public function test_sample(){
-        require "Sample.php";
+        require "./src/result/Sample.php";
         $test = new Person();
         $this->assertEquals('Hello World', $test->greeting());
     }
